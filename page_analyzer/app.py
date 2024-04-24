a30ea0fd48e6bfd918417f357a9bc792157df9a6
Ivlev Nikita <thel4stofme@yandex.ru>
2024-04-24 18:38:45 +0300

    Add context managers for DB connections

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -25,8 +25,7 @@ def normalize_url(url):
 
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
-connection = psycopg2.connect(DATABASE_URL)
+database_url = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -53,117 +52,122 @@ def add_url():
             flash_messages=get_flashed_messages(with_categories=True)
             )
     else:
-        with connection.cursor() as cursor:
-            query = 'SELECT * FROM urls WHERE name=%s;'
-            cursor.execute(query, (url,))
+        with psycopg2.connect(database_url) as connection:
+            with connection.cursor() as cursor:
+                query = 'SELECT * FROM urls WHERE name=%s;'
+                cursor.execute(query, (url,))
 
-            if cursor.fetchall():
-                flash('Страница уже существует', 'info')
-            else:
-                query = 'INSERT INTO urls (name, created_at) VALUES (%s, %s);'
+                if cursor.fetchall():
+                    flash('Страница уже существует', 'info')
+                else:
+                    query = 'INSERT INTO urls (name, created_at) VALUES (%s, %s);'
+                    cursor.execute(
+                        query,
+                        (url, datetime.now()))
+
+                    connection.commit()
+                    flash('Страница успешно добавлена', 'success')
+
+                query = 'SELECT id FROM urls WHERE name = %s;'
                 cursor.execute(
                     query,
-                    (url, datetime.now()))
+                    (url,)
+                    )
 
-                connection.commit()
-                flash('Страница успешно добавлена', 'success')
-
-            query = 'SELECT id FROM urls WHERE name = %s;'
-            cursor.execute(
-                query,
-                (url,)
-                )
-
-            id = cursor.fetchone()[0]
+                id = cursor.fetchone()[0]
         return redirect(url_for('url_page', id=id))
 
 
 @app.route('/urls')
 def urls_list():
-    with connection.cursor() as cursor:
-        query = """SELECT
-                   DISTINCT ON (urls.id)
-                       urls.id,
-                       urls.name,
-                       url_checks.created_at,
-                       url_checks.status_code
-                  FROM urls LEFT JOIN url_checks
-                  ON urls.id = url_checks.url_id
-                  ORDER BY urls.id DESC, url_checks.url_id DESC;"""
-        cursor.execute(query)
+    with psycopg2.connect(database_url) as connection:
+        with connection.cursor() as cursor:
+            query = """SELECT
+                    DISTINCT ON (urls.id)
+                        urls.id,
+                        urls.name,
+                        url_checks.created_at,
+                        url_checks.status_code
+                    FROM urls LEFT JOIN url_checks
+                    ON urls.id = url_checks.url_id
+                    ORDER BY urls.id DESC, url_checks.url_id DESC;"""
+            cursor.execute(query)
 
-        return render_template(
-            'urls.html',
-            flash_messages=get_flashed_messages(with_categories=True),
-            urls=cursor.fetchall()
-            )
+            return render_template(
+                'urls.html',
+                flash_messages=get_flashed_messages(with_categories=True),
+                urls=cursor.fetchall()
+                )
 
 
 @app.route('/urls/<id>')
 def url_page(id):
-    with connection.cursor() as cursor:
-        query = 'SELECT * FROM urls WHERE id=%s;'
-        cursor.execute(query, (id,))
-        response = cursor.fetchall()
-        if not response:
-            abort(404)
-        id, url, created_at = response[0]
+    with psycopg2.connect(database_url) as connection:
+        with connection.cursor() as cursor:
+            query = 'SELECT * FROM urls WHERE id=%s;'
+            cursor.execute(query, (id,))
+            response = cursor.fetchall()
+            if not response:
+                abort(404)
+            id, url, created_at = response[0]
 
-        query = """SELECT
-                        id,
-                        status_code,
-                        h1,
-                        title,
-                        description,
-                        created_at
-                   FROM url_checks
-                   WHERE url_id = %s
-                   ORDER BY id DESC;"""
-        cursor.execute(query, (id,))
+            query = """SELECT
+                            id,
+                            status_code,
+                            h1,
+                            title,
+                            description,
+                            created_at
+                    FROM url_checks
+                    WHERE url_id = %s
+                    ORDER BY id DESC;"""
+            cursor.execute(query, (id,))
 
-        checks = cursor.fetchall()
-        return render_template(
-            'url.html',
-            flash_messages=get_flashed_messages(with_categories=True),
-            id=id,
-            url=url,
-            created_at=created_at,
-            checks=checks
-        )
+            checks = cursor.fetchall()
+            return render_template(
+                'url.html',
+                flash_messages=get_flashed_messages(with_categories=True),
+                id=id,
+                url=url,
+                created_at=created_at,
+                checks=checks
+            )
 
 
 @app.route('/urls/<id>/checks', methods=['POST'])
 def conduct_check(id):
-    with connection.cursor() as cursor:
-        query = 'SELECT name FROM urls WHERE id=%s;'
-        cursor.execute(query, (id,))
-        try:
-            response = requests.get(cursor.fetchall()[0][0])
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'html.parser')
-            description_tag = soup.find('meta', attrs={'name': 'description'})
+    with psycopg2.connect(database_url) as connection:
+        with connection.cursor() as cursor:
+            query = 'SELECT name FROM urls WHERE id=%s;'
+            cursor.execute(query, (id,))
+            try:
+                response = requests.get(cursor.fetchall()[0][0])
+                response.raise_for_status()
+                soup = BeautifulSoup(response.text, 'html.parser')
+                description_tag = soup.find('meta',
+                                            attrs={'name': 'description'})
 
-            query = """INSERT INTO
-                       url_checks (
-                       url_id, created_at, status_code, title, h1, description
-                       )
-                       VALUES (%s, %s, %s, %s, %s, %s);"""
-            cursor.execute(
-                query,
-                (
-                    id,
-                    datetime.now(),
-                    response.status_code,
-                    soup.title.string if soup.title else None,
-                    soup.h1.string if soup.h1 else None,
-                    description_tag['content'] if description_tag else None
-                    )
-            )
+                query = """INSERT INTO
+                        url_checks (
+                        url_id, created_at, status_code, title, h1, description
+                        )
+                        VALUES (%s, %s, %s, %s, %s, %s);"""
+                cursor.execute(
+                    query,
+                    (
+                        id,
+                        datetime.now(),
+                        response.status_code,
+                        soup.title.string if soup.title else None,
+                        soup.h1.string if soup.h1 else None,
+                        description_tag['content'] if description_tag else None
+                        )
+                )
 
-            connection.commit()
-            flash('Страница успешно проверена', 'success')
-        except Exception:
-            flash('Произошла ошибка при проверке', 'error')
+                connection.commit()
+                flash('Страница успешно проверена', 'success')
+            except Exception:
+                flash('Произошла ошибка при проверке', 'error')
 
     return redirect(url_for('url_page', id=id))
 
