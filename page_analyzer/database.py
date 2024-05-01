fbac1262b4fdcd43bd71e59bfb86881e53bba690
Ivlev Nikita <thel4stofme@yandex.ru>
2024-05-02 00:52:48 +0300

    Only commit to DB if changes were made

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -9,38 +9,41 @@ load_dotenv()
 CONNTECTION = psycopg2.connect(os.getenv('DATABASE_URL'))
 
 
-def use_cursor(function):
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        with CONNTECTION.cursor() as cursor:
-            result = function(*args, cursor=cursor, **kwargs)
-            CONNTECTION.commit()
-            return result
+def use_cursor(changing_data=False):
+    def wrapper(function):
+        @wraps(function)
+        def inner(*args, **kwargs):
+            with CONNTECTION.cursor() as cursor:
+                result = function(*args, cursor=cursor, **kwargs)
+                if changing_data:
+                    CONNTECTION.commit()
+                return result
+        return inner
     return wrapper
 
 
-@use_cursor
+@use_cursor()
 def is_url_recorded(url, cursor):
     query = 'SELECT * FROM urls WHERE name=%s;'
     cursor.execute(query, (url,))
     return bool(cursor.fetchall())
 
 
-@use_cursor
+@use_cursor(changing_data=True)
 def add_url(url, cursor) -> int:
     query = 'INSERT INTO urls (name) VALUES (%s) RETURNING id;'
     cursor.execute(query, (url,))
     return cursor.fetchone()[0]
 
 
-@use_cursor
+@use_cursor()
 def get_url_id(url, cursor):
     query = 'SELECT id FROM urls WHERE name = %s;'
     cursor.execute(query, (url,))
     return cursor.fetchone()[0]
 
 
-@use_cursor
+@use_cursor()
 def get_urls_with_checks(cursor):
     query = """SELECT
             DISTINCT ON (urls.id)
@@ -55,14 +58,14 @@ def get_urls_with_checks(cursor):
     return cursor.fetchall()
 
 
-@use_cursor
+@use_cursor()
 def get_url_by_id(id, cursor):
     query = 'SELECT * FROM urls WHERE id=%s;'
     cursor.execute(query, (id,))
     return cursor.fetchone()
 
 
-@use_cursor
+@use_cursor()
 def get_checks(id, cursor):
     query = """SELECT
                     id,
@@ -78,7 +81,7 @@ def get_checks(id, cursor):
     return cursor.fetchall()
 
 
-@use_cursor
+@use_cursor(changing_data=True)
 def add_check(url_id, status_code, title, h1, description, cursor):
     query = """INSERT INTO
                 url_checks (
